bc91491b33c1a60efcc1706c3e81fd30236877fc
Eric Lemanissier <eric.lemanissier@gmail.com>
2019-12-27 12:06:39 +0100

    fix macos CI
    reusing @Croydon 's work for https://github.com/bincrafters/templates

--- .travis/install.sh
+++ .travis/install.sh
@@ -3,20 +3,26 @@
 set -e
 set -x
 
+pip install --upgrade pip
+python --version
+pip --version
 if [[ "$(uname -s)" == 'Darwin' ]]; then
-    brew update || brew update
-    brew outdated pyenv || brew upgrade pyenv
-    brew install pyenv-virtualenv
-    brew install cmake || true
-
-    if which pyenv > /dev/null; then
-        eval "$(pyenv init -)"
-    fi
-
-    pyenv install 2.7.10
-    pyenv virtualenv 2.7.10 conan
-    pyenv rehash
-    pyenv activate conan
+  rm -rf /usr/local/opt/pyenv;
+  rm /usr/local/bin/pyenv;
+  rm /usr/local/bin/pyenv-install;
+  rm /usr/local/bin/pyenv-uninstall;
+  ln -s /usr/local/opt/pyenv/libexec/pyenv /usr/local/bin/pyenv;
+  unset PYENV_ROOT;
+  curl -L https://github.com/pyenv/pyenv-installer/raw/master/bin/pyenv-installer | bash;
+  export PATH="$HOME/.pyenv/bin:$PATH";
+  eval "$(pyenv init -)";
+  eval "$(pyenv virtualenv-init -)";
+  pyenv --version;
+  pyenv install 3.7.5;
+  pyenv virtualenv 3.7.5 conan;
+  pyenv rehash;
+  pyenv activate conan;
+  pip install cmake --upgrade;
 fi
 
 pip install conan --upgrade
